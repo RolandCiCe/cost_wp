34b382f65a391672e8ed4fa2088e7788b02b620b
Milton Pineda <mapineda.13@gmail.com>
2018-03-05 23:19:21 -0600

    desde rama milton quienes somos

--- wp-content/themes/costplantilla/page-quienes-somos.php
+++ wp-content/themes/costplantilla/page-quienes-somos.php
@@ -1,21 +1,110 @@
 <?php get_header(); ?>
 
-  <?php while(have_posts()): the_post(); ?>
+<section>
+  <div class="">
+    <img src="<?php echo get_template_directory_uri().'/img/quienes_somos/home_quienes_somos/icono_quienes_somos2.png' ; ?>" alt="" />
+  </div>
 
-    <div class="hero" style="background-image:url(<?php echo get_the_post_thumbnail_url(); ?>);">
-      <div class="contenido-hero">
-        <div class="texto-hero">
-          <?php the_title('<h1>','</h1>'); ?>
-        </div>
-      </div>
+  <div class="texto-hero">
+    <h1>QUIENES SOMOS?</h1>
+  </div>
+  <div class="">
+    <p class="rojo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta mollis luctus. Vestibulum ullamcorper enim sagittis pharetra pretium. Suspendisse eros tellus, eleifend eu egestas eget, tempor quis sem. Nulla sit amet elementum turpis. Etiam at elit eleifend, hendrerit velit vitae, scelerisque erat. Fusce ac sem eu tortor ornare ornare. Sed elementum interdum turpis. Phasellus gravida ante ipsum, sit amet euismod felis facilisis sit amet. Etiam sit amet malesuada lectus. Pellentesque lobortis sagittis ex. Etiam vestibulum ut nisi ac iaculis. Donec nec blandit mi. Ut non rhoncus quam. Etiam non libero non felis vehicula posuere. Aliquam in ex massa. Quisque lobortis magna non nisi ultricies venenatis.</p>
+  </div>
+</section>
+
+<section>
+  <div class="texto-hero">
+    <h2>HISTORIA</h2>
+  </div>
+  <div class="">
+    <img src="<?php echo get_template_directory_uri().'/img/quienes_somos/home_quienes_somos/imag01.png' ; ?>" alt="" />
+  </div>
+  <div class="texto-hero">
+    <h3>CoST</h3>
+  </div>
+  <div class="">
+    <p class="rojo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta mollis luctus. Vestibulum ullamcorper enim sagittis pharetra pretium. Suspendisse eros tellus, eleifend eu egestas eget, tempor quis sem. Nulla sit amet elementum turpis. Etiam at elit eleifend, hendrerit velit vitae, scelerisque erat. Fusce ac sem eu tortor ornare ornare. Sed elementum interdum turpis. Phasellus gravida ante ipsum, sit amet euismod felis facilisis sit amet. Etiam sit amet malesuada lectus. Pellentesque lobortis sagittis ex. Etiam vestibulum ut nisi ac iaculis. Donec nec blandit mi. Ut non rhoncus quam. Etiam non libero non felis vehicula posuere. Aliquam in ex massa. Quisque lobortis magna non nisi ultricies venenatis.</p>
+  </div>
+  <div class="">
+    <p class="rojo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta mollis luctus. Vestibulum ullamcorper enim sagittis pharetra pretium. Suspendisse eros tellus, eleifend eu egestas eget, tempor quis sem. Nulla sit amet elementum turpis. Etiam at elit eleifend, hendrerit velit vitae, scelerisque erat. Fusce ac sem eu tortor ornare ornare. Sed elementum interdum turpis. Phasellus gravida ante ipsum, sit amet euismod felis facilisis sit amet. Etiam sit amet malesuada lectus. Pellentesque lobortis sagittis ex. Etiam vestibulum ut nisi ac iaculis. Donec nec blandit mi. Ut non rhoncus quam. Etiam non libero non felis vehicula posuere. Aliquam in ex massa. Quisque lobortis magna non nisi ultricies venenatis.</p>
+  </div>
+  <div class="">
+    <img src="<?php echo get_template_directory_uri().'/img/quienes_somos/home_quienes_somos/imag02.png' ; ?>" alt="" />
+  </div>
+  <div class="">
+    <p class="rojo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta mollis luctus. Vestibulum ullamcorper enim sagittis pharetra pretium. Suspendisse eros tellus, eleifend eu egestas eget, tempor quis sem. Nulla sit amet elementum turpis. Etiam at elit eleifend, hendrerit velit vitae, scelerisque erat. Fusce ac sem eu tortor ornare ornare. Sed elementum interdum turpis. Phasellus gravida ante ipsum, sit amet euismod felis facilisis sit amet. Etiam sit amet malesuada lectus. Pellentesque lobortis sagittis ex. Etiam vestibulum ut nisi ac iaculis. Donec nec blandit mi. Ut non rhoncus quam. Etiam non libero non felis vehicula posuere. Aliquam in ex massa. Quisque lobortis magna non nisi ultricies venenatis.</p>
+  </div>
+  <div class="">
+    <p class="rojo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta mollis luctus. Vestibulum ullamcorper enim sagittis pharetra pretium. Suspendisse eros tellus, eleifend eu egestas eget, tempor quis sem. Nulla sit amet elementum turpis. Etiam at elit eleifend, hendrerit velit vitae, scelerisque erat. Fusce ac sem eu tortor ornare ornare. Sed elementum interdum turpis. Phasellus gravida ante ipsum, sit amet euismod felis facilisis sit amet. Etiam sit amet malesuada lectus. Pellentesque lobortis sagittis ex. Etiam vestibulum ut nisi ac iaculis. Donec nec blandit mi. Ut non rhoncus quam. Etiam non libero non felis vehicula posuere. Aliquam in ex massa. Quisque lobortis magna non nisi ultricies venenatis.</p>
+  </div>
+</section>
+
+<section>
+  <div class="">
+    <h1>CUALES SON LOS PROCESOS ESENCIALES DE CoST?</h1>
+  </div>
+  <div class="divulgacion">
+    <div class="">
+      <img src="<?php echo get_template_directory_uri().'/img/quienes_somos/home_quienes_somos/icono_divulgacion.png' ; ?>" alt="" />
     </div>
-
-    <div class="principal contenedor">
-      <main class="texto-centrado contenido-paginas">
-          <?php the_content(); ?>
-      </main>
-
+    <div class="">
+      <p>Divulgaion</p>
     </div>
-  <?php endwhile; ?>
+    <div class="">
+      <p class="rojo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta mollis luctus. Vestibulum ullamcorper enim sagittis pharetra pretium. Suspendisse eros tellus, eleifend eu egestas eget, tempor quis sem. Nulla sit amet elementum turpis. Etiam at elit eleifend, hendrerit velit vitae, scelerisque erat. Fusce ac sem eu tortor ornare ornare. Sed elementum interdum turpis. Phasellus gravida ante ipsum, sit amet euismod felis facilisis sit amet. Etiam sit amet malesuada lectus. Pellentesque lobortis sagittis ex. Etiam vestibulum ut nisi ac iaculis. Donec nec blandit mi. Ut non rhoncus quam. Etiam non libero non felis vehicula posuere. Aliquam in ex massa. Quisque lobortis magna non nisi ultricies venenatis.</p>
+    </div>
+    <div class="">
+      <a href="/cost_wp/index.php/divulgacion/">Leer mas</a>
+    </div>
+  </div>
+
+  <div class="aseguramiento">
+    <div class="">
+      <img src="<?php echo get_template_directory_uri().'/img/quienes_somos/home_quienes_somos/icono_aseguramiento.png' ; ?>" alt="" />
+    </div>
+    <div class="">
+      <p>aseguramiento</p>
+    </div>
+    <div class="">
+      <p class="rojo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta mollis luctus. Vestibulum ullamcorper enim sagittis pharetra pretium. Suspendisse eros tellus, eleifend eu egestas eget, tempor quis sem. Nulla sit amet elementum turpis. Etiam at elit eleifend, hendrerit velit vitae, scelerisque erat. Fusce ac sem eu tortor ornare ornare. Sed elementum interdum turpis. Phasellus gravida ante ipsum, sit amet euismod felis facilisis sit amet. Etiam sit amet malesuada lectus. Pellentesque lobortis sagittis ex. Etiam vestibulum ut nisi ac iaculis. Donec nec blandit mi. Ut non rhoncus quam. Etiam non libero non felis vehicula posuere. Aliquam in ex massa. Quisque lobortis magna non nisi ultricies venenatis.</p>
+    </div>
+    <div class="">
+      <a href="/cost_wp/index.php/aseguramiento/">Leer mas</a>
+    </div>
+  </div>
+
+  <div class="demanda-ciudadana">
+    <div class="">
+      <img src="<?php echo get_template_directory_uri().'/img/quienes_somos/home_quienes_somos/icono_demanda_ciudadana.png' ; ?>" alt="" />
+    </div>
+    <div class="">
+      <p>Demanda ciudadana de rendicion de cuentas</p>
+    </div>
+    <div class="">
+      <p class="rojo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta mollis luctus. Vestibulum ullamcorper enim sagittis pharetra pretium. Suspendisse eros tellus, eleifend eu egestas eget, tempor quis sem. Nulla sit amet elementum turpis. Etiam at elit eleifend, hendrerit velit vitae, scelerisque erat. Fusce ac sem eu tortor ornare ornare. Sed elementum interdum turpis. Phasellus gravida ante ipsum, sit amet euismod felis facilisis sit amet. Etiam sit amet malesuada lectus. Pellentesque lobortis sagittis ex. Etiam vestibulum ut nisi ac iaculis. Donec nec blandit mi. Ut non rhoncus quam. Etiam non libero non felis vehicula posuere. Aliquam in ex massa. Quisque lobortis magna non nisi ultricies venenatis.</p>
+    </div>
+    <div class="">
+      <a href="/cost_wp/index.php/demanda-ciudadana/">Leer mas</a>
+    </div>
+  </div>
+</section>
+
+<section>
+  <div class="">
+    <h2>QUE GANA NUESTRO PAIS CON EL TRABAJO DE CoST EL SALVADOR?</h2>
+  </div>
+  <div class="">
+    <p class="rojo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta mollis luctus. Vestibulum ullamcorper enim sagittis pharetra pretium. Suspendisse eros tellus, eleifend eu egestas eget, tempor quis sem. Nulla sit amet elementum turpis. Etiam at elit eleifend, hendrerit velit vitae, scelerisque erat. Fusce ac sem eu tortor ornare ornare. Sed elementum interdum turpis. Phasellus gravida ante ipsum, sit amet euismod felis facilisis sit amet. Etiam sit amet malesuada lectus. Pellentesque lobortis sagittis ex. Etiam vestibulum ut nisi ac iaculis. Donec nec blandit mi. Ut non rhoncus quam. Etiam non libero non felis vehicula posuere. Aliquam in ex massa. Quisque lobortis magna non nisi ultricies venenatis.</p>
+  </div>
+  <div class="">
+    <div class="">
+      <h2>NUESTRO LEMA:</h2>
+    </div>
+    <div class="">
+      <p class="rojo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta mollis luctus. Vestibulum ullamcorper enim sagittis pharetra pretium. Suspendisse eros tellus, eleifend eu egestas eget, tempor quis sem. Nulla sit amet elementum turpis. Etiam at elit eleifend, hendrerit velit vitae, scelerisque erat. Fusce ac sem eu tortor ornare ornare. Sed elementum interdum turpis. Phasellus gravida ante ipsum, sit amet euismod felis facilisis sit amet. Etiam sit amet malesuada lectus. Pellentesque lobortis sagittis ex. Etiam vestibulum ut nisi ac iaculis. Donec nec blandit mi. Ut non rhoncus quam. Etiam non libero non felis vehicula posuere. Aliquam in ex massa. Quisque lobortis magna non nisi ultricies venenatis.</p>
+    </div>
+  </div>
+</section>
 
 <?php get_footer();?>
